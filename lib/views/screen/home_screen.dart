1ab407125ceb831caf7855c637c8bb7e85a2ce47
YusufEbinc <86497681+YusufEbinc@users.noreply.github.com>
2021-09-23 13:21:19 +0300

    supabaseAdd

--- lib/views/screen/home_screen.dart
+++ lib/views/screen/home_screen.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_book_app/constant/app_constant.dart';
+import 'package:flutter_book_app/model/my_book.dart';
+import 'package:supabase/supabase.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  late SupabaseClient client;
+  late MyBook mybook;
+
+  var allBook = <MyBook>[];
+
+  @override
+  void initState() {
+    super.initState();
+    getAllBook();
+    client = SupabaseClient(AppConstant.supabaseUrl, AppConstant.supabaseKey);
+  }
+
+  getAllBook() async {}
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return const Scaffold(
+      body: Center(
+        child: Text('kvmsdlikvmds'),
+      ),
     );
   }
 }
